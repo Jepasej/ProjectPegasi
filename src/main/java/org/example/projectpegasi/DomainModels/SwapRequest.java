f969e4fef77a47aa937f283598e0accd360198de
Sunisima <lsun8525@gmail.com>
2025-05-19 14:11:22 +0200

    Setting up swapRequst

--- src/main/java/org/example/projectpegasi/DomainModels/SwapRequest.java
+++ src/main/java/org/example/projectpegasi/DomainModels/SwapRequest.java
@@ -1,5 +1,62 @@
 package org.example.projectpegasi.DomainModels;
 
+import java.util.Date;
+
 public class SwapRequest
 {
+    private int profileBId;
+    private int matchId;
+    private int stateId;
+    private Date matchDate;
+    private Date matchDateResponse;
+
+    private int profileAId;
+
+    public int getProfileAId() {
+        return profileAId;
+    }
+
+    public void setProfileAId(int profileAId) {
+        this.profileAId = profileAId;
+    }
+
+    public int getProfileBId() {
+        return profileBId;
+    }
+
+    public void setProfileBId(int profileBId) {
+        this.profileBId = profileBId;
+    }
+
+    public int getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(int matchId) {
+        this.matchId = matchId;
+    }
+
+    public int getStateId() {
+        return stateId;
+    }
+
+    public void setStateId(int stateId) {
+        this.stateId = stateId;
+    }
+
+    public Date getMatchDate() {
+        return matchDate;
+    }
+
+    public void setMatchDate(Date matchDate) {
+        this.matchDate = matchDate;
+    }
+
+    public Date getMatchDateResponse() {
+        return matchDateResponse;
+    }
+
+    public void setMatchDateResponse(Date matchDateResponse) {
+        this.matchDateResponse = matchDateResponse;
+    }
 }
