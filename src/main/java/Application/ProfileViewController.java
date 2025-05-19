bc1d7bc766ec83f84ce3b909a93cd4636d7dae32
TobiasFalck <TobiasFalck13@gmail.com>
2025-05-19 12:44:51 +0200

    init

--- src/main/java/Application/ProfileViewController.java
+++ src/main/java/Application/ProfileViewController.java
@@ -1,5 +1,22 @@
 package Application;
 
-public class ProfileViewController
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ProfileViewController extends Application
 {
+
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ProfileView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        stage.setScene(scene);
+        stage.setTitle("Profile View");
+        stage.show();
+    }
 }
