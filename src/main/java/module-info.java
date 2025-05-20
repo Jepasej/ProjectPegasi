6c46880df0860457cd3d612c58f391aec3b650f9
Jepasej <146959451+Jepasej@users.noreply.github.com>
2025-05-20 10:32:46 +0200

    potentially implemented verifyUser

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.projectpegasi {
     requires javafx.controls;
     requires javafx.fxml;
     requires jdk.compiler;
+    requires java.sql;
 
 
     opens org.example.projectpegasi.Application to javafx.fxml, javafx.graphics;
--- src/main/java/org/example/projectpegasi/Persistence/DataAccessObject.java
+++ src/main/java/org/example/projectpegasi/Persistence/DataAccessObject.java
@@ -2,6 +2,11 @@ package org.example.projectpegasi.Persistence;
 
 import org.example.projectpegasi.DomainModels.User;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DataAccessObject implements DAO
 {
     @Override
@@ -40,7 +45,25 @@ public class DataAccessObject implements DAO
         //TO BE IMPLEMENTED
         //take user
         //run stored provedure
-        //return whether input matches record.
-        return false;
+        //return whether input matches record. CheckPassword
+
+        //SKAL OPDATERES TIL VORES SINGLETON CONNECTION.
+        Connection conn = null;
+
+        String sql = "{call CheckPassword(?,?)}";
+        try
+        {
+
+            CallableStatement cs = conn.prepareCall(sql);
+            cs.setString(1, user.getUserName());
+            cs.setString(2, user.getPassword());
+            ResultSet rs = cs.executeQuery();
+            return rs.next();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 }
