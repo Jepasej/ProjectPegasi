e7b852605512ab401b37f2af1e385396784db1ee
Sunisima <lsun8525@gmail.com>
2025-05-21 10:27:25 +0200

    Makin requestView work in UI

--- src/main/java/org/example/projectpegasi/Application/IncomingRequestViewController.java
+++ src/main/java/org/example/projectpegasi/Application/IncomingRequestViewController.java
@@ -1,5 +1,15 @@
 package org.example.projectpegasi.Application;
 
+import javafx.event.ActionEvent;
+
 public class IncomingRequestViewController
 {
+    public void onMatchesButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onOutgoingRequestsButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onIncomingRequestsButtonClick(ActionEvent actionEvent) {
+    }
 }
