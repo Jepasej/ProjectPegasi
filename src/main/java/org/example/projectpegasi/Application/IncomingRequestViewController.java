3d9b5539a50813005b2dc449584ba0387e618642
Maj-Britt Deigaard <deigaardm@gmail.com>
2025-05-21 10:33:47 +0200

    onOutgoingRequestsButtonClick go til view

--- src/main/java/org/example/projectpegasi/Application/IncomingRequestViewController.java
+++ src/main/java/org/example/projectpegasi/Application/IncomingRequestViewController.java
@@ -1,5 +1,17 @@
 package org.example.projectpegasi.Application;
 
+import org.example.projectpegasi.BusinessService.ControllerNames;
+import org.example.projectpegasi.HelloApplication;
+
 public class IncomingRequestViewController
 {
+
+
+    public void onOutgoingRequestsButtonClick()
+    {
+        HelloApplication.changeScene(ControllerNames.OutgoingRequestView);
+    }
 }
+
+
+
