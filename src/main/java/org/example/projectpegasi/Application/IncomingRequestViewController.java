a094a65ac98202e082d56e61189a774647b6d1a8
Maj-Britt Deigaard <deigaardm@gmail.com>
2025-05-21 10:48:48 +0200

    Made onActionButtons work in IncomingRequestView.fxml

--- src/main/java/org/example/projectpegasi/Application/IncomingRequestViewController.java
+++ src/main/java/org/example/projectpegasi/Application/IncomingRequestViewController.java
@@ -8,17 +8,18 @@ import org.example.projectpegasi.HelloApplication;
 public class IncomingRequestViewController
 {
 
-
+    public void onMatchesButtonClick()
+    {
+        HelloApplication.changeScene(ControllerNames.MatchView);
+    }
 
     public void onOutgoingRequestsButtonClick()
     {
         HelloApplication.changeScene(ControllerNames.OutgoingRequestView);
     }
 
-    public void onMatchesButtonClick(ActionEvent actionEvent) {
-    }
-
-
-    public void onIncomingRequestsButtonClick(ActionEvent actionEvent) {
+    public void onBackToProfileButtonClick()
+    {
+        HelloApplication.changeScene(ControllerNames.ProfileView);
     }
 }
