edf2a4ca7292eba394481bc8f911e6f7a175f4f4
Jepasej <146959451+Jepasej@users.noreply.github.com>
2025-05-28 09:14:42 +0200

    dummy geoLocator created.

--- src/main/java/org/example/projectpegasi/BusinessService/GeoLocator.java
+++ src/main/java/org/example/projectpegasi/BusinessService/GeoLocator.java
@@ -1,5 +1,11 @@
 package org.example.projectpegasi.BusinessService;
 
+import org.example.projectpegasi.DomainModels.Profile;
+
 public class GeoLocator
 {
+    public double getDistance(Profile p1, Profile p2)
+    {
+        return 5.00;
+    }
 }
