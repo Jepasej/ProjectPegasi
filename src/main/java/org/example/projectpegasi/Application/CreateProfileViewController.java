8eeb6b7d3899091f3a5bbc705ca37d366f4cce05
Jepasej <146959451+Jepasej@users.noreply.github.com>
2025-05-30 10:04:38 +0200

    CreateProfileViewController now writes jobfunction to DB

--- src/main/java/org/example/projectpegasi/Application/CreateProfileViewController.java
+++ src/main/java/org/example/projectpegasi/Application/CreateProfileViewController.java
@@ -1,7 +1,5 @@
 package org.example.projectpegasi.Application;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
